a12eaa00ffd2c99fb96dc93156573ef6bed9c6d5
Alexander Kell <Alexander.Kell@rapiddweller.com>
2024-12-10 09:06:10 +0100

    Change license to MIT
    Updated license in all source files from Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License to MIT License. Updated contact information for questions and support to info@rapiddweller.com.

--- datamimic_ce/constants/convention_constants.py
+++ datamimic_ce/constants/convention_constants.py
@@ -1,7 +1,7 @@
 # DATAMIMIC
 # Copyright (c) 2023-2024 Rapiddweller Asia Co., Ltd.
-# Licensed under the Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License (CC BY-NC-SA 4.0).
-# For commercial use, please contact Rapiddweller at info@rapiddweller.com to obtain a commercial license.
-# Full license text available at: http://creativecommons.org/licenses/by-nc-sa/4.0/
+# This software is licensed under the MIT License.
+# See LICENSE file for the full text of the license.
+# For questions and support, contact: info@rapiddweller.com
 
 NAME_SEPARATOR = "|"
