17b14a83fa949a8fb9c9293331f0ae5f88345a94
Tung Lieu <tung.lieu@rapiddweller.com>
2025-01-13 14:57:45 +0700

    add test - fix later

--- tests_ce/functional_tests/test_memstore/test_memstore.py
+++ tests_ce/functional_tests/test_memstore/test_memstore.py
@@ -21,3 +21,35 @@ class TestMemStore:
         result = test_engine.capture_result()
         assert len(result["data"]) == 100000
         assert len(result["data2"]) == len(result["data"])
+
+    def test_memstore_in_nested_generate_with_mp(self):
+        test_engine = DataMimicTest(
+            test_dir=self._test_dir, filename="test_memstore_in_nested_generate_with_mp.xml", capture_test_result=True
+        )
+        test_engine.test_with_timer()
+        result = test_engine.capture_result()
+
+        wrapping = result["wrapping"]
+        assert len(wrapping) == 2
+
+        data_1 = result["iterate_1"]
+        assert len(data_1) == 20
+        count_data_1 = 0
+        for element_data_1 in data_1:
+            count_data_1 += 1
+            if count_data_1 > 10:
+                assert element_data_1["id"] == f"2_{count_data_1 - 10}"
+            else:
+                assert element_data_1["id"] == f"1_{count_data_1}"
+            assert isinstance(element_data_1["name"], str)
+
+        data_2 = result["iterate_2"]
+        assert len(data_2) == 20
+        count_data_2 = 0
+        for element_data_2 in data_2:
+            count_data_2 += 1
+            if count_data_2 > 10:
+                assert element_data_2["id"] == f"2_{count_data_2 - 10}"
+            else:
+                assert element_data_2["id"] == f"1_{count_data_2}"
+            assert isinstance(element_data_2["number"], int)
