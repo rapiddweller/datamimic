c56aa77b51b28d27f615b76ee3cab2082c88a9da
dangsg <44063361+dangsg@users.noreply.github.com>
2025-02-27 12:53:58 +0700

    Add factory test feature (#64)
    Co-authored-by: Dang Ly <dang.ly@rapiddweller.com>

---
+++ tests_ce/factory_test/test_customer/customer_factory.py
@@ -0,0 +1,8 @@
+from pathlib import Path
+
+from datamimic_ce.factory.datamimic_test_factory import DataMimicTestFactory
+
+
+class CustomerFactory(DataMimicTestFactory):
+    def __init__(self, xml_path: Path, entity_name: str):
+        super().__init__(xml_path, entity_name)
