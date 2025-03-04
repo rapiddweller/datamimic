897aa853b1c2195568f12a0545de4af037d9245d
Tung Lieu <tung.lieu@rapiddweller.com>
2025-03-04 19:55:07 +0700

    update test case

--- tests_ce/functional_tests/test_constraints/test_constraints.py
+++ tests_ce/functional_tests/test_constraints/test_constraints.py
@@ -20,3 +20,13 @@ class TestConstraints(TestCase):
         engine = DataMimicTest(test_dir=self._test_dir, filename="test_constraints.xml", capture_test_result=True)
         engine.test_with_timer()
         result = engine.capture_result()
+
+        synthetic_customers = result["synthetic_customers"]
+        assert len(synthetic_customers) == 100
+        for ele in synthetic_customers:
+            if ele["credit_score"] < 600:
+                assert ele["risk_profile"] == 'High'
+            elif 600 <= ele["credit_score"] < 750:
+                assert ele["risk_profile"] == 'Medium'
+            else:
+                assert ele["risk_profile"] == 'Low'
