51500513266a28f9f81b31c4c40dacd74a7204b1
Tung Lieu <tung.lieu@rapiddweller.com>
2025-03-19 12:29:46 +0700

    add order entity functional test

--- tests_ce/functional_tests/test_entity_functional/test_entity_functional.py
+++ tests_ce/functional_tests/test_entity_functional/test_entity_functional.py
@@ -3,13 +3,15 @@
 # This software is licensed under the MIT License.
 # See LICENSE file for the full text of the license.
 # For questions and support, contact: info@rapiddweller.com
-
-
+import datetime
 from pathlib import Path
 
 from datamimic_ce.data_mimic_test import DataMimicTest
 
 import pandas as pd
+
+from datamimic_ce.domains.common.models import Address
+
 
 class TestEntityFunctional:
     _test_dir = Path(__file__).resolve().parent
@@ -52,6 +54,24 @@ class TestEntityFunctional:
         us_product = result["us_product"]
         assert len(us_product) == 50
         self._check_product(us_product)
+
+    def test_functional_entity_order(self) -> None:
+        engine = DataMimicTest(
+            test_dir=self._test_dir,
+            filename="functional_test_entity_order.xml",
+            capture_test_result=True)
+        engine.test_with_timer()
+
+        result = engine.capture_result()
+        assert result
+
+        default_order = result["default_order"]
+        assert len(default_order) == 50
+        self._check_order(default_order)
+
+        us_order = result["us_order"]
+        assert len(us_order) == 50
+        self._check_order(us_order)
 
     def _check_product(self, products):
         """
@@ -118,3 +138,86 @@ class TestEntityFunctional:
             assert category.lower().replace('_', ' ') in tags
             assert brand.lower() in tags
             assert condition.lower() in tags if condition != "NEW" else condition.lower() not in tags
+
+    def _check_order(self, orders):
+        """
+        helper of test case test_functional_entity_order
+        """
+        ecommerce_data_dir = Path(__file__).resolve().parents[3] / "datamimic_ce/domain_data/ecommerce"
+        status_list = pd.read_csv(ecommerce_data_dir.joinpath("order_statuses_US.csv"))["status"].tolist()
+        payment_methods_list = pd.read_csv(ecommerce_data_dir.joinpath("payment_methods_US.csv"))["method"].tolist()
+        shipping_methods_list = pd.read_csv(ecommerce_data_dir.joinpath("shipping_methods_US.csv"))["method"].tolist()
+        currencies_list = pd.read_csv(ecommerce_data_dir.joinpath("currencies_US.csv"))["code"].tolist()
+
+        for order in orders:
+            assert "order_id" in order
+            order_id = order["order_id"]
+            assert isinstance(order_id, str)
+            assert order_id.startswith("ORD")
+            assert len(order_id) == len("ORD") + 8
+
+            assert "user_id" in order
+            user_id = order["user_id"]
+            assert isinstance(user_id, str)
+            assert user_id.startswith("USER")
+            assert len(user_id) == len("USER")+8
+
+            assert "date" in order
+            assert isinstance(order["date"], datetime.datetime)
+
+            assert "status" in order
+            assert isinstance(order["status"], str)
+            assert order["status"] in status_list
+
+            assert "payment_method" in order
+            assert isinstance(order["payment_method"], str)
+            assert order["payment_method"] in payment_methods_list
+
+            assert "shipping_method" in order
+            assert isinstance(order["shipping_method"], str)
+            assert order["shipping_method"] in shipping_methods_list
+
+            assert "shipping_address" in order
+            assert isinstance(order["shipping_address"], str)
+
+            assert "billing_address" in order
+            assert isinstance(order["billing_address"], str)
+
+            assert "currency" in order
+            assert isinstance(order["currency"], str)
+            assert order["currency"] in currencies_list
+
+            assert "tax_amount" in order
+            assert isinstance(order["tax_amount"], float)
+            assert order["tax_amount"] > 0.0
+
+            assert "shipping_amount" in order
+            assert isinstance(order["shipping_amount"], float)
+            assert order["shipping_amount"] >= 0.0
+
+            assert "discount_amount" in order
+            assert isinstance(order["discount_amount"], float)
+            assert order["discount_amount"] >= 0.0
+
+            assert "coupon_code" in order
+            assert isinstance(order["coupon_code"], (str, type(None)))
+            if order["coupon_code"]:
+                assert any(coupon_prefixes in order["coupon_code"] for coupon_prefixes in ["SAVE", "DISCOUNT", "SPECIAL", "PROMO", "DEAL"])
+
+            assert "notes" in order
+            assert isinstance(order["notes"], (str, type(None)))
+            if order["notes"]:
+                assert order["notes"] in ["Please leave at the front door",
+                                          "Call before delivery",
+                                          "Gift - please don't include receipt",
+                                          "Fragile items - handle with care",
+                                          "Please deliver after 5pm",
+                                          "Ring doorbell upon delivery",
+                                          "Contact customer before shipping",
+                                          "Include gift message",
+                                          "Expedite if possible",
+                                          "Address has a gate code: 1234",]
+
+            assert "total_amount" in order
+            assert isinstance(order["total_amount"], float)
+            assert order["total_amount"] > (order["tax_amount"] + order["shipping_amount"] - order["discount_amount"])
