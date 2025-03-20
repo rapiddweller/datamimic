cdf149ea1ad362a359edaebea922243dd0d2f637
Tung Lieu <tung.lieu@rapiddweller.com>
2025-03-20 15:42:42 +0700

    add cascade test

--- tests_ce/functional_tests/test_constraints/test_constraints.py
+++ tests_ce/functional_tests/test_constraints/test_constraints.py
@@ -160,3 +160,22 @@ class TestConstraints(TestCase):
             assert False
         except ValueError as e:
             assert "then: Value error, must be not empty" in str(e)
+
+    def test_cascaded_constraints(self):
+        engine = DataMimicTest(test_dir=self._test_dir,
+                               filename="test_cascaded_constraints.xml",
+                               capture_test_result=True)
+        engine.test_with_timer()
+        result = engine.capture_result()
+
+        synthetic_customers = result["synthetic_customers"]
+        assert len(synthetic_customers) == 10000
+        for ele in synthetic_customers:
+            assert isinstance(ele["id"], int)
+            assert ele["id"] in range(1, 10001)
+            if ele["credit_score"] < 600:
+                assert ele["risk_profile"] == 'High'
+            elif 600 <= ele["credit_score"] < 750:
+                assert ele["risk_profile"] == 'Medium'
+            else:
+                assert ele["risk_profile"] == 'Low'
