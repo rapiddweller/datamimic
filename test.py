f4585c540cf29de87e552830df400f46fd879172
Alexander Kell <Alexander.Kell@rapiddweller.com>
2025-03-25 16:45:08 +0700

    Remove obsolete test files and XML configuration for customer data generation
    - Deleted `test.py` which contained customer data generation logic.
    - Removed `test.xml` that defined the XML setup for generating customer data.
    
    This cleanup eliminates outdated files that are no longer relevant to the current data generation framework.

--- test.py
+++
@@ -1,8 +0,0 @@
-from datamimic_ce.factory.datamimic_test_factory import DataMimicTestFactory
-
-customer_factory = DataMimicTestFactory("test.xml", "customer")
-customer = customer_factory.create()
-
-print(customer["id"])  # 1
-print(customer["first_name"])  # Jose
-print(customer["last_name"])  # Ayers
